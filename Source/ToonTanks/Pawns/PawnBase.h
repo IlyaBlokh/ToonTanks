c4f231cbb4db47888cf8bbe822ed43418a55546a
Ilya Blokh <ilyablokh@gmail.com>
2020-07-26 23:22:07 +0500

    pawn tank class

--- Source/ToonTanks/Pawns/PawnBase.h
+++ Source/ToonTanks/Pawns/PawnBase.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Ilya Blokh
 
 #pragma once
 
@@ -25,15 +25,5 @@ private:
 public:
 	// Sets default values for this pawn's properties
 	APawnBase();
-
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 
 };

37796882c6981f47d8575819c607cbb988783b33
Ilya Blokh <ilyablokh@gmail.com>
2020-07-30 16:48:38 +0500

    game mode class and bp

---
+++ Source/ToonTanks/GameModes/TankGameModeBase.cpp
@@ -0,0 +1,22 @@
+// Copyright Ilya Blokh
+
+
+#include "TankGameModeBase.h"
+
+void ATankGameModeBase::BeginPlay()
+{
+}
+
+void ATankGameModeBase::ActorDied(AActor* DeadActor)
+{
+
+}
+
+void ATankGameModeBase::HandleGameStart()
+{
+}
+
+void ATankGameModeBase::HandleGameOver(bool PlayerWon)
+{
+}
+
